1ae4024e48f91487a5fe25f2bafcf5ea2d11b7fc
iseeteaj# <iseetea@gmail.com>
2017-07-31 20:46:58 +0100

    Adding ToDos

--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -1,9 +1,20 @@
+var bodyParser = require('body-parser')
+
 var toDos = require('../models/ToDos.js')
 
+var urlencodedParser = bodyParser.urlencoded({extended: false})
+
 function toDoController (app) {
-app.get('/', function (req, res){
-    res.render('home', {
-        toDos: toDos.getItems()
+    app.get('/', function (req, res){
+        res.render('home', {
+            toDos: toDos.getItems()
+        })
+    app.post('/', urlencodedParser, function(req, res){
+        toDos.addItem(req.body.item)
+
+        res.render('home', {
+            toDos: toDos.getItems()
+        })
     })
 })
 }
